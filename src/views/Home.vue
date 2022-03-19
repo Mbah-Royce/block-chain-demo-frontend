a41a37e18fcc8c89b1dfad1eb490af10bd85d6e7
tolos123 <alemchu123@yahoo.com>
2022-03-19 20:13:24 +0100

    set title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home d-flex justify-content-center">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <HelloWorld msg="Welcome to Blockchain Demo " />
   </div>
 </template>
 
