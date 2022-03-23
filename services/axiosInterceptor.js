9d9508afacd549cb484a01213c6d0120f1c85c45
tolos123 <alemchu123@yahoo.com>
2022-03-23 13:48:42 +0100

    fix url

--- services/axiosInterceptor.js
+++ services/axiosInterceptor.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import store from "../src/store/index";
-import { GET_USER_TOKEN_GETTER } from '../src/store/storeConstants';
+import { GET_USER_TOKEN_GETTER } from "../src/store/storeConstants";
 const axiosInstance = axios.create({
-    // baseURL: "https://block-chain-demo-backend.herokuapp.com/api/v1/",
-    baseURL: "http://localhost:8000/api/v1/",
+    baseURL: "https://block-chain-demo-backend.herokuapp.com/api/v1/",
+    // baseURL: "http://localhost:8000/api/v1/",
     headers: {
         Accept: "application/json"
     }
